1bce902ce7cb9ea281c9ccefbb97c8e1808e552c
6ichy <richymanzo19@gmail.com>
2025-11-05 01:49:52 -0800

    changed tab name

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     canonical: '/'
   },
   title: {
-    default: 'Richy Manzo | Portfolio',
+    default: 'Portfolio | Richy Manzo | Vercel',
     template: '%s | Nim'
   },
   description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
